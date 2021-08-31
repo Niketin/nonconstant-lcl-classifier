91dff52d17b5493e1af797da28868a43c8b7a251
Niketin <Niketin@users.noreply.github.com>
2021-09-01 01:14:49 +0300

    Add support for generating simple biregular graphs

---
+++ src/lcl_problem/encoding.rs
@@ -0,0 +1,43 @@
+use std::{collections::HashMap, error::Error};
+
+#[derive(Debug)]
+pub struct Encoding {
+    pub data: Vec<u8>,
+    pub size: (usize, usize),
+}
+
+impl Encoding {
+    pub fn from_str(
+        encoding: &str,
+        symbol_map: &mut HashMap<String, u8>,
+    ) -> Result<Self, Box<dyn Error>> {
+        let mut lines = encoding.lines();
+        let first_line = lines.next();
+        let width = first_line.unwrap().split_ascii_whitespace().count();
+
+        let all_same_length = lines.all(|ref l| l.split_ascii_whitespace().count() == width);
+        assert!(all_same_length);
+
+        let mut v = Vec::<u8>::new();
+        for line in encoding.lines() {
+            for symbol in line.split_ascii_whitespace() {
+                let value = if symbol_map.contains_key(symbol) {
+                    symbol_map.get(symbol).unwrap().clone()
+                } else {
+                    let new_value = symbol_map.len() as u8 + 1;
+                    symbol_map.insert(String::from(symbol), new_value);
+                    new_value
+                };
+
+                v.push(value)
+            }
+        }
+
+        let height = encoding.lines().count();
+
+        Ok(Encoding {
+            data: v,
+            size: (width, height),
+        })
+    }
+}
